610904aa1ef56f49c845b88d86521a770663d03f
Bindu Prakash <bindu2305@gmail.com>
2019-03-13 06:42:53 +0000

    added messages and current user state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
+function TopBar(){
+  return(
+    <nav className="navbar">
+    <a href="/" className="navbar-brand">Chatty</a>
+    </nav>);
+}
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {name: "Bob"},
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+          id: 1
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+          id: 2
+        }
+
+      ]
+    };
+  }
+
+
   render() {
+    const addMessage = message => {
+      let messages = this.state.messages;
+      messages = messages.concat([
+        {
+          id: messages.length + 1,
+          username: message.username,
+          content: message.content
+        }
+      ]);
+      this.setState({messages});
+    };
     return (
-      <h1>Hello React :)</h1>
+    <div>
+      <TopBar/>
+      <MessageList messages={this.state.messages}/>
+      <ChatBar currentUser={this.state.currentUser} addMessage={addMessage}/>
+    </div>
     );
   }
 }
