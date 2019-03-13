3b2a81538ff2defab6cb4c9de41419f0e0a98302
Bindu Prakash <bindu2305@gmail.com>
2019-03-13 06:46:07 +0000

    added message list for each message

---
+++ src/MessageList.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+class MessageList extends Component {
+  render(){
+  const messageList = this.props.messages.map((message)=>{
+    return(<Message message={message} key={message.id}/>);
+  });
+    return(
+    <main className="messages">
+    {messageList}
+
+    </main>);
+
+  }
+}
+export default MessageList;
